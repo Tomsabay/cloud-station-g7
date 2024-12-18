44895bf5e64712ef187ffbc631a1e895c3fcc4aa
abay <2370413596@qq.com>
2024-12-18 21:56:13 +0800

    第一个测试

---
+++ example/oss_test.go
@@ -0,0 +1,52 @@
+package example_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+)
+
+var (
+	client *oss.Client
+)
+
+var (
+	Endpoint        = os.Getenv("ENDPOINT")
+	AccessKeyId     = os.Getenv("ACCESS_KEY_ID")
+	AccessKeySecret = os.Getenv("ACCESS_KEY_SECRET"))
+	BucketName      = os.Getenv("BUCKET_NAME")
+)
+
+func TestBucketList(t *testing.T) {
+	lsRes, err := client.ListBuckets()
+	if err != nil {
+		t.Log(err)
+	}
+
+	for _, bucket := range lsRes.Buckets {
+		fmt.Println("Buckets:", bucket.Name)
+	}
+}
+
+func TestUploadFile(t *testing.T) {
+
+	bucket, err := client.Bucket("my-bucket")
+	if err != nil {
+		t.Log(err)
+	}
+
+	err = bucket.PutObjectFromFile("my-object", "LocalFile")
+	if err != nil {
+		// HandleError(err)
+	}
+}
+
+func init() {
+	c, err := oss.New("Endpoint", "AccessKeyId", "AccessKeySecret")
+	if err != nil {
+		panic(err)
+	}
+	client = c
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Tomsabay/cloud-station-g7
+
+go 1.23.3
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v3.0.2+incompatible // indirect
+	golang.org/x/time v0.8.0 // indirect
+)
